7f6f60f608251f91fee6deb6ec2d0a44c28c15c7
Patrick Tagatsi (pt237p) <pt237p@att.com>
2020-06-18 21:25:04 -0400

    Header bar

---
+++ Templates/template_header.php
@@ -0,0 +1,12 @@
+<header>
+    <div id="topHeaderBleuDiv">
+        <div id="socialLogoLeftDiv">
+            <ul id="socialMediaUl">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no">
+
+        <meta charset="utf-8"/>
+
+        <link rel = "stylesheet" type = "text/css" href = "CSS/style.css">
+        <link rel = "stylesheet" type = "text/css" href = "CSS/header.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+
+        <title>Centre Hospitalier Saint-Pierre</title>
+    </head>
+    <body>
+        <div class = "wrapper">
+            <?php include_once("Templates/template_header.php");?>
+        </div>
+    </body>
+</html>
